6c59853471d606ee6a446e6ece6fa10ce388b6cd
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-10 21:58:23 +0100

    Create selectCategory.js

---
+++ handlers/selectCategory.js
@@ -0,0 +1,29 @@
+const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, EmbedBuilder } = require('discord.js');
+const dictionary = require('../data/dictionary.json');
+
+module.exports = {
+    customId: 'select_category',
+    async execute(interaction) {
+        const categories = Object.keys(dictionary);
+
+        const categoryOptions = categories.map(category => 
+            new StringSelectMenuOptionBuilder()
+                .setLabel(category)
+                .setValue(category)
+        );
+
+        const categorySelectMenu = new StringSelectMenuBuilder()
+            .setCustomId('select_category')
+            .setPlaceholder('Select a category')
+            .addOptions(categoryOptions);
+
+        const row = new ActionRowBuilder().addComponents(categorySelectMenu);
+
+        const embed = new EmbedBuilder()
+            .setTitle('Boom Dictionary Categories')
+            .setDescription('Select a category to view terms.')
+            .setColor('#0099ff');
+
+        await interaction.update({ embeds: [embed], components: [row] });
+    }
+};
