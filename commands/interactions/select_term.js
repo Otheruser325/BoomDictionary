32e9085eee90a0af8de4c383dcbe8688e4487228
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-10 22:05:12 +0100

    Create select_term.js

---
+++ commands/interactions/select_term.js
@@ -0,0 +1,35 @@
+const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, EmbedBuilder } = require('discord.js');
+const dictionary = require('../data/dictionary.json');
+
+module.exports = {
+    customId: 'select_term',
+    async execute(interaction) {
+        const selectedCategory = interaction.values[0];
+        const terms = dictionary[selectedCategory];
+
+        if (!terms) {
+            await interaction.reply({ content: 'Category not found!', ephemeral: true });
+            return;
+        }
+
+        const termOptions = Object.keys(terms).map(term => 
+            new StringSelectMenuOptionBuilder()
+                .setLabel(term)
+                .setValue(term)
+        );
+
+        const termSelectMenu = new StringSelectMenuBuilder()
+            .setCustomId('select_term')
+            .setPlaceholder('Select a term')
+            .addOptions(termOptions);
+
+        const row = new ActionRowBuilder().addComponents(termSelectMenu);
+
+        const embed = new EmbedBuilder()
+            .setTitle(`Terms in ${selectedCategory}`)
+            .setDescription('Select a term to view its definition.')
+            .setColor('#0099ff');
+
+        await interaction.update({ embeds: [embed], components: [row] });
+    }
+};
