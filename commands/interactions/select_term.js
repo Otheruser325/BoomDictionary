a1628552b2d75c903baa88bfc1535e0af4d0354e
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-10 22:09:55 +0100

    Update select_term.js

--- commands/interactions/select_term.js
+++ commands/interactions/select_term.js
@@ -12,24 +12,37 @@ module.exports = {
             return;
         }
 
-        const termOptions = Object.keys(terms).map(term => 
-            new StringSelectMenuOptionBuilder()
-                .setLabel(term)
-                .setValue(term)
-        );
+        if (terms[interaction.values[0]]) {
+            const term = interaction.values[0];
+            const definition = terms[term];
 
-        const termSelectMenu = new StringSelectMenuBuilder()
-            .setCustomId('select_term')
-            .setPlaceholder('Select a term')
-            .addOptions(termOptions);
+            const embed = new EmbedBuilder()
+                .setTitle(`Boom Dictionary: ${term}`)
+                .setDescription(definition)
+                .addFields({ name: 'Category', value: selectedCategory })
+                .setColor('#0099ff');
 
-        const row = new ActionRowBuilder().addComponents(termSelectMenu);
+            await interaction.update({ embeds: [embed], components: [] });
+        } else {
+            const termOptions = Object.keys(terms).map(term =>
+                new StringSelectMenuOptionBuilder()
+                    .setLabel(term)
+                    .setValue(term)
+            );
 
-        const embed = new EmbedBuilder()
-            .setTitle(`Terms in ${selectedCategory}`)
-            .setDescription('Select a term to view its definition.')
-            .setColor('#0099ff');
+            const termSelectMenu = new StringSelectMenuBuilder()
+                .setCustomId('select_term')
+                .setPlaceholder('Select a term')
+                .addOptions(termOptions);
 
-        await interaction.update({ embeds: [embed], components: [row] });
+            const row = new ActionRowBuilder().addComponents(termSelectMenu);
+
+            const embed = new EmbedBuilder()
+                .setTitle(`Terms in ${selectedCategory}`)
+                .setDescription('Select a term to view its definition.')
+                .setColor('#0099ff');
+
+            await interaction.update({ embeds: [embed], components: [row] });
+        }
     }
 };
