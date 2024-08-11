07d335e2e63914a7af56d24f11d470bde49b5c96
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-11 20:22:50 +0100

    Delete commands/prefix/test.js

--- commands/prefix/test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'test',
-    description: 'A test command',
-    aliases: [], // Optional
-    async execute(message, args) {
-        await message.reply('Test command executed!');
-    }
-};
