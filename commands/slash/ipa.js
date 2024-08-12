867d4151ccc624ced23d3b3a01f4de5d4193469c
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-12 18:41:40 +0100

    Update ipa.js

--- commands/slash/ipa.js
+++ commands/slash/ipa.js
@@ -1,6 +1,4 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
-const path = require('path');
-const fs = require('fs');
 const dictionary = require('../../data/dictionary.json');
 const BASE_URL = 'https://funny-eclair-d437ee.netlify.app';
 
@@ -34,35 +32,25 @@ module.exports = {
                     .join(' ')
                     + '.mp3';
 
-                const mp3FilePath = path.join(__dirname, '../../pronunciations', formattedFileName);
-
                 const embed = new EmbedBuilder()
                     .setTitle(`Pronunciation for ${terminology || term}`)
                     .setDescription(`Pronunciation: ${pronunciation || 'Not provided'}`)
                     .setColor('#0099ff');
 
-                if (fs.existsSync(mp3FilePath)) {
-                    // URL encode the file name
-                    const mp3URL = `${BASE_URL}/${encodeURIComponent(formattedFileName)}`;
+                const mp3URL = `${BASE_URL}/${encodeURIComponent(formattedFileName)}`;
 
-                    const components = [
-                        new ActionRowBuilder()
-                            .addComponents(
-                                new ButtonBuilder()
-                                    .setCustomId('play_pronunciation')
-                                    .setLabel('Play Pronunciation')
-                                    .setStyle(ButtonStyle.Primary),
-                                new ButtonBuilder()
-                                    .setURL(mp3URL)
-                                    .setLabel('Download MP3')
-                                    .setStyle(ButtonStyle.Link)
-                            )
-                    ];
+                const components = new ActionRowBuilder().addComponents(
+                    new ButtonBuilder()
+                        .setCustomId(`play_pronunciation_${term}`)
+                        .setLabel('Play Pronunciation')
+                        .setStyle(ButtonStyle.Primary),
+                    new ButtonBuilder()
+                        .setURL(mp3URL)
+                        .setLabel('Download MP3')
+                        .setStyle(ButtonStyle.Link)
+                );
 
-                    await interaction.reply({ embeds: [embed], components: components });
-                } else {
-                    await interaction.reply({ content: 'This pronunciation file is currently unavailable.', ephemeral: true });
-                }
+                await interaction.reply({ embeds: [embed], components: [components] });
 
                 pronunciationFound = true;
                 break;
