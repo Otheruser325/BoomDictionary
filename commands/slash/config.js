fed79aec483fd927b3fe7f243cdaf0e97ca34236
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-12 19:09:58 +0100

    Update config.js

--- commands/slash/config.js
+++ commands/slash/config.js
@@ -11,7 +11,13 @@ module.exports = {
                 .setRequired(true)),
     async execute(interaction) {
         const channel = interaction.options.getChannel('channel');
-        
+
+        // Check if the user has the necessary permissions
+        const member = interaction.member;
+        if (!member.permissions.has('ADMINISTRATOR') && !member.permissions.has('MANAGE_CHANNELS')) {
+            return interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+        }
+
         if (channel.type !== 'GUILD_VOICE') {
             return interaction.reply({ content: 'Please select a valid voice channel.', ephemeral: true });
         }
