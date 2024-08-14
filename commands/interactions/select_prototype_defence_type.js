9f7f3a556965dd5c26ab9c80a9bfcac328bdb218
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-14 02:11:50 +0100

    Create select_prototype_defence_type.js

---
+++ commands/interactions/select_prototype_defence_type.js
@@ -0,0 +1,43 @@
+const { EmbedBuilder } = require('discord.js');
+const prototypeDefences = require('../../data/prototypeDefences.json');
+const { formatNumber } = require('../../utils/formatNumber');
+
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction) {
+        if (!interaction.isStringSelectMenu()) return;
+
+        const { customId, values } = interaction;
+
+        if (customId === 'select_prototype_defence_type') {
+            const selectedDefenceType = values[0];
+            const defenceTypeKey = selectedDefenceType.toLowerCase();
+            const mappedDefenceType = {
+                'doom cannon': 'doom_cannon',
+                'shock blaster': 'shock_blaster',
+                'lazor beam': 'lazor_beam',
+                'hot pot': 'hot_pot',
+                'shield generator': 'shield_generator',
+                'damage amplifier': 'damage_amplifier'
+            }[defenceTypeKey];
+
+            const defenceData = prototypeDefences[mappedDefenceType];
+
+            if (!defenceData) {
+                return interaction.reply({ content: 'No data found for the selected prototype defence type.', ephemeral: true });
+            }
+
+            const embed = new EmbedBuilder()
+                .setTitle(`${defenceData.name}`)
+                .setDescription(defenceData.description || 'No description available.')
+                .addFields(
+                    { name: 'Max Level', value: defenceData.maxLevel.toString(), inline: true },
+                    { name: 'Attack Speed', value: defenceData.attackSpeed.toString(), inline: true },
+                    { name: 'Range', value: defenceData.range.toString(), inline: true }
+                )
+                .setColor('#0099ff');
+
+            await interaction.reply({ embeds: [embed], ephemeral: true });
+        }
+    }
+};
