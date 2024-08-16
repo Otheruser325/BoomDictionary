ccd2994673bd4638d6794b277d709ff54f60e3ab
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-16 18:50:51 +0100

    Update select_troop_level.js

--- commands/interactions/select_troop_level.js
+++ commands/interactions/select_troop_level.js
@@ -46,7 +46,7 @@ module.exports = {
                 { name: 'Training Time', value: troopData.trainingTime || 'Unknown', inline: true },
                 { name: 'Movement Speed', value: troopData.movementSpeed || 'Unknown', inline: true },
                 { name: 'Attack Range', value: `${formatNumber(range)} Tiles`, inline: true },
-                { name: 'Attack Speed', value: attackSpeed !== 'Unknown' ? `${formatNumber(attackSpeed)} ms` : 'Unknown', inline: true },
+                { name: 'Attack Speed', value: attackSpeed !== 'Unknown' ? `${formatNumber(attackSpeed)}ms` : 'Unknown', inline: true },
                 { name: 'Armory Level Required', value: armoryRequired.toString(), inline: true }
             )
             .setColor('#0099ff');
