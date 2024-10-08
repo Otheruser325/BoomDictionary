99ecb30dd4ec099fcf9ea28477ab2d2b32e4396f
Otheruser325 <btdbmodder@gmail.com>
2024-10-08 16:40:27 +0100

    Update defence.js

--- commands/prefix/defence.js
+++ commands/prefix/defence.js
@@ -23,108 +23,121 @@ module.exports = {
     usage: '<defence_type> <level>',
 
     async execute(message, args) {
-        if (args.length === 0) {
-            const defenceOptions = Object.keys(validDefenceTypes).map(defenceKey => {
-                const defence = defences[validDefenceTypes[defenceKey]];
-                const description = (defence && defence.description) ? defence.description.substring(0, 100) : 'No description available.';
-                return new StringSelectMenuOptionBuilder()
-                    .setLabel(defenceKey.charAt(0).toUpperCase() + defenceKey.slice(1))
-                    .setValue(validDefenceTypes[defenceKey]) // Use the internal key
-                    .setDescription(description);
-            });
+		// Check bot permissions
+        const botPermissions = message.channel.permissionsFor(message.guild.members.me);
+        const requiredPermissions = new PermissionsBitField(['SendMessages', 'ViewChannel', 'ReadMessageHistory']);
 
-            const selectMenu = new StringSelectMenuBuilder()
-                .setCustomId('select_defence_type')
-                .setPlaceholder('Select a defence type')
-                .addOptions(defenceOptions);
+        if (!botPermissions.has(requiredPermissions)) {
+            return message.reply("I don't have the necessary permissions to execute this command. Please make sure I have `SEND_MESSAGES`, `VIEW_CHANNEL`, and `READ_MESSAGE_HISTORY` permissions.");
+        }
 
-            const row = new ActionRowBuilder().addComponents(selectMenu);
+        try {
+            if (args.length === 0) {
+                const defenceOptions = Object.keys(validDefenceTypes).map(defenceKey => {
+                    const defence = defences[validDefenceTypes[defenceKey]];
+                    const description = (defence && defence.description) ? defence.description.substring(0, 100) : 'No description available.';
+                    return new StringSelectMenuOptionBuilder()
+                        .setLabel(defenceKey.charAt(0).toUpperCase() + defenceKey.slice(1))
+                        .setValue(validDefenceTypes[defenceKey])
+                        .setDescription(description);
+                });
 
-            const embed = new EmbedBuilder()
-                .setTitle('Select a Defence Type')
-                .setDescription('Please choose a defence type to view its details.')
-                .setColor('#0099ff');
+                const selectMenu = new StringSelectMenuBuilder()
+                    .setCustomId('select_defence_type')
+                    .setPlaceholder('Select a defence type')
+                    .addOptions(defenceOptions);
 
-            await message.channel.send({ embeds: [embed], components: [row] });
-        } else {
-            const userFriendlyDefenceType = args.slice(0, -1).join(' ').toLowerCase().trim();
-            const level = parseInt(args[args.length - 1], 10);
+                const row = new ActionRowBuilder().addComponents(selectMenu);
 
-            const defenceType = validDefenceTypes[userFriendlyDefenceType];
+                const embed = new EmbedBuilder()
+                    .setTitle('Select a Defence Type')
+                    .setDescription('Please choose a defence type to view its details.')
+                    .setColor('#0099ff');
 
-            if (!defenceType) {
-                return message.reply(`Invalid defence type! Available types are: ${Object.keys(validDefenceTypes).join(', ')}.`);
-            }
-
-            const defenceData = defences[defenceType];
-
-            if (!defenceData) {
-                return message.reply('No data found for the provided defence type.');
-            }
-
-            if (isNaN(level) || level < 1 || level > defenceData.maxLevel) {
-                return message.reply(`Invalid level! Please provide a level between 1 and ${defenceData.maxLevel}.`);
-            }
-
-            const levelData = defenceData.levels[level];
-            if (!levelData) {
-                return message.reply(`No data available for level ${level}.`);
-            }
-
-            const stats = levelData.stats;
-            const upgradeCost = levelData.upgradeCost || { wood: 0, stone: 0, iron: 0 };
-            const attackSpeed = defenceData.attackSpeed || 'Unknown';
-            const range = defenceData.range || 'Unknown';
-            const hqRequired = levelData.hqRequired || 'N/A';
-            const image = levelData.image || '';
-
-            // Calculate DPS if attackSpeed is known
-            const dps = attackSpeed !== 'Unknown' ? (stats.damage / (attackSpeed / 1000)).toFixed(2) : 'Unknown';
-
-            const embed = new EmbedBuilder()
-                .setTitle(`${defenceData.name} - Level ${level}`)
-                .setDescription(defenceData.description || 'No description available.')
-                .setColor('#0099ff');
-				
-			if (image) {
-                embed.setThumbnail(image);
-            }
-
-            // Handle unique stats for certain defences
-            if (defenceType === 'shock_launcher') {
-                embed.addFields(
-                    { name: 'Health', value: formatNumber(stats.health), inline: true },
-                    { name: 'DPS', value: formatNumber(dps), inline: true },
-                    { name: 'Damage Per Shot', value: formatNumber(stats.damage), inline: true },
-                    { name: 'Range', value: `${formatNumber(range)} Tiles`, inline: true },
-                    { name: 'Attack Speed', value: attackSpeed !== 'Unknown' ? `${formatNumber(attackSpeed)}ms` : 'Unknown', inline: true },
-                    { name: 'Shock Duration', value: `${formatNumber(stats.stunDuration)} seconds`, inline: true },
-                    { name: 'Upgrade Cost', value: `Wood: ${formatNumber(upgradeCost.wood)}\nStone: ${formatNumber(upgradeCost.stone)}\nIron: ${formatNumber(upgradeCost.iron)}`, inline: true },
-                    { name: 'Upgrade Time', value: `${levelData.upgradeTime || 'Not available'}`, inline: true },
-                    { name: 'HQ Required', value: hqRequired.toString(), inline: true }
-                );
+                await message.channel.send({ embeds: [embed], components: [row] });
             } else {
-                // Handle general defence stats
-                let dps = 'N/A';
-                let damagePerShot = 'N/A';
+                const userFriendlyDefenceType = args.slice(0, -1).join(' ').toLowerCase().trim();
+                const level = parseInt(args[args.length - 1], 10);
 
-                if (stats.damage !== null) {
-                    dps = (stats.damage / (defenceData.attackSpeed / 1000)).toFixed(2);
-                    damagePerShot = stats.damage.toString();
+                const defenceType = validDefenceTypes[userFriendlyDefenceType];
+
+                if (!defenceType) {
+                    return message.reply(`Invalid defence type! Available types are: ${Object.keys(validDefenceTypes).join(', ')}.`);
                 }
-                embed.addFields(
-                    { name: 'Health', value: formatNumber(stats.health), inline: true },
-                    { name: 'DPS', value: formatNumber(dps), inline: true },
-                    { name: 'Damage Per Shot', value: formatNumber(stats.damage), inline: true },
-                    { name: 'Range', value: `${formatNumber(range)} Tiles`, inline: true },
-                    { name: 'Attack Speed', value: attackSpeed !== 'Unknown' ? `${formatNumber(attackSpeed)}ms` : 'Unknown', inline: true },
-                    { name: 'Upgrade Cost', value: `Wood: ${formatNumber(upgradeCost.wood)}\nStone: ${formatNumber(upgradeCost.stone)}\nIron: ${formatNumber(upgradeCost.iron)}`, inline: true },
-                    { name: 'Upgrade Time', value: `${levelData.upgradeTime || 'Not available'}`, inline: true },
-                    { name: 'HQ Required', value: hqRequired.toString(), inline: true }
-                );
-            }
 
-            await message.channel.send({ embeds: [embed] });
+                const defenceData = defences[defenceType];
+
+                if (!defenceData) {
+                    return message.reply('No data found for the provided defence type.');
+                }
+
+                if (isNaN(level) || level < 1 || level > defenceData.maxLevel) {
+                    return message.reply(`Invalid level! Please provide a level between 1 and ${defenceData.maxLevel}.`);
+                }
+
+                const levelData = defenceData.levels[level];
+                if (!levelData) {
+                    return message.reply(`No data available for level ${level}.`);
+                }
+
+                const stats = levelData.stats || {};
+                const upgradeCost = levelData.upgradeCost || { wood: 0, stone: 0, iron: 0 };
+                const attackSpeed = defenceData.attackSpeed || 'Unknown';
+                const range = defenceData.range || 'Unknown';
+                const hqRequired = levelData.hqRequired || 'N/A';
+                const image = levelData.image || '';
+
+                // Calculate DPS if attackSpeed is known
+                const dps = attackSpeed !== 'Unknown' ? (stats.damage / (attackSpeed / 1000)).toFixed(2) : 'Unknown';
+
+                const embed = new EmbedBuilder()
+                    .setTitle(`${defenceData.name} - Level ${level}`)
+                    .setDescription(defenceData.description || 'No description available.')
+                    .setColor('#0099ff');
+				
+			    if (image) {
+                    embed.setThumbnail(image);
+                }
+
+                // Handle unique stats for certain defences
+                if (defenceType === 'shock_launcher') {
+                embed.addFields(
+                        { name: 'Health', value: formatNumber(stats.health), inline: true },
+                        { name: 'DPS', value: formatNumber(dps), inline: true },
+                        { name: 'Damage Per Shot', value: formatNumber(stats.damage), inline: true },
+                        { name: 'Range', value: `${formatNumber(range)} Tiles`, inline: true },
+                        { name: 'Attack Speed', value: attackSpeed !== 'Unknown' ? `${formatNumber(attackSpeed)}ms` : 'Unknown', inline: true },
+                        { name: 'Shock Duration', value: `${formatNumber(stats.stunDuration)} seconds`, inline: true },
+                        { name: 'Upgrade Cost', value: `Wood: ${formatNumber(upgradeCost.wood)}\nStone: ${formatNumber(upgradeCost.stone)}\nIron: ${formatNumber(upgradeCost.iron)}`, inline: true },
+                        { name: 'Upgrade Time', value: `${levelData.upgradeTime || 'Not available'}`, inline: true },
+                        { name: 'HQ Required', value: hqRequired.toString(), inline: true }
+                    );
+                } else {
+                    // Handle general defence stats
+                    let dps = 'N/A';
+                    let damagePerShot = 'N/A';
+
+                    if (stats.damage !== null) {
+                        dps = (stats.damage / (defenceData.attackSpeed / 1000)).toFixed(2);
+                        damagePerShot = stats.damage.toString();
+                    }
+                    embed.addFields(
+                        { name: 'Health', value: formatNumber(stats.health), inline: true },
+                        { name: 'DPS', value: formatNumber(dps), inline: true },
+                        { name: 'Damage Per Shot', value: formatNumber(stats.damage), inline: true },
+                        { name: 'Range', value: `${formatNumber(range)} Tiles`, inline: true },
+                        { name: 'Attack Speed', value: attackSpeed !== 'Unknown' ? `${formatNumber(attackSpeed)}ms` : 'Unknown', inline: true },
+                        { name: 'Upgrade Cost', value: `Wood: ${formatNumber(upgradeCost.wood)}\nStone: ${formatNumber(upgradeCost.stone)}\nIron: ${formatNumber(upgradeCost.iron)}`, inline: true },
+                        { name: 'Upgrade Time', value: `${levelData.upgradeTime || 'Not available'}`, inline: true },
+                        { name: 'HQ Required', value: hqRequired.toString(), inline: true }
+                    );
+                }
+
+                await message.channel.send({ embeds: [embed] });
+			}
+        } catch (error) {
+            console.error('Error executing defence command:', error);
+            message.reply('An error occurred while executing this command. Please try again later.');
         }
     }
 };
